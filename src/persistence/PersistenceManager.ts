f85f8303b37c2656fc29266006e33a2806f2fb8f
Sebastian Alasia <72724740+TianSeb@users.noreply.github.com>
2024-04-27 19:57:00 -0300

    dotenv

--- src/persistence/PersistenceManager.ts
+++ src/persistence/PersistenceManager.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose'
 import createError from 'http-errors'
+import { config } from '../config/Config'
 
 export const initMongoDb = async (): Promise<void> => {
   mongoose
-    .connect("mongodb+srv://alebriuk:123456zxcv@yoiki.dfwuwcw.mongodb.net/?retryWrites=true&w=majority&appName=yoiki")
+    .connect(config.mongoUri || '')
     .then(() => {
       console.log('Mongo Db connection successful')
     })
